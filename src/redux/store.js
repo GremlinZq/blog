eab8e4c8e9bd7e289faef326458753ab856e689d
Daniil <you@example.com>
2021-10-15 06:07:43 +0300

    CREATED: added articleReducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,11 @@ import {applyMiddleware, combineReducers, createStore} from "redux";
 import middleware from 'redux-thunk';
 import {reducer as formReducer } from 'redux-form';
 import authReducer from "./reducers/auth-reducer";
+import articleReducer from "./reducers/article-reducer";
 
 const reducers = combineReducers({
     auth: authReducer,
+    articles: articleReducer,
     form: formReducer
 })
 
