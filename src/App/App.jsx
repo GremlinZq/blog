6ad475766a402f1529f1c7ba24043353ea34343a
Daniil <you@example.com>
2021-10-15 06:05:18 +0300

    FIX: fix App

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,7 @@
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import 'antd/dist/antd.css';
 import Header from "../Components/Header/Header";
-import ArticleList from "../Components/ArticleList/ArticleList";
-import Login from "../Components/AuthorizationForm/AuthorizationForm";
-import ArticleListItem from "../Components/ArticleList/ArticleListItem/ArticleListItem";
-
+import Routes from "../routes/Routes";
 import './App.scss';
 
 const App = () => {
@@ -12,14 +9,7 @@ const App = () => {
         <div className='app'>
             <Router>
                 <Header />
-
-                <Switch>
-                    <Route exact path='/' component={ArticleList}/>
-                    <Route path='/articles/:userId' component={ArticleListItem}/>
-                    <Route path='/signin' component={Login}/>
-                    <Route path='/signup' component={Login}/>
-                </Switch>
-
+                <Routes />
             </Router>
         </div>
     )
