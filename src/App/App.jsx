1b9e59d4faea637664c3820e88eba9da000bc18c
Daniil <you@example.com>
2021-11-01 11:17:23 +0300

    Fix

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,18 +1,25 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {initializeApplication} from "../redux/reducers/app-reducer";
+import {Spin} from "antd";
 import 'antd/dist/antd.css';
-import Header from "../Components/Header/Header";
+import {Header} from "../Components/Header/Header";
 import Routes from "../routes/Routes";
 import './App.scss';
 
-const App = () => {
-    return (
-        <div className='app'>
-            <Router>
-                <Header />
-                <Routes />
-            </Router>
-        </div>
-    )
-}
+export const App = () => {
+    const initialized = useSelector(state => state.app.applicationInitialized);
+    const isLoggedIn = useSelector(state => state.auth.isLoggedIn);
+    const dispatch = useDispatch();
 
-export default App;
+    useEffect(() => {
+        dispatch(initializeApplication(isLoggedIn))
+    }, [dispatch,isLoggedIn])
+
+    return !initialized
+        ? <Spin style={{position: 'absolute', top: '50%', left: '50%'}} size='large'/>
+        : <div className='app'>
+            <Header/>
+            <Routes/>
+        </div>
+}
