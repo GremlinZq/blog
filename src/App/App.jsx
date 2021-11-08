40425ac63348e9eb1936d0c64faabfd4362b8fe2
Daniil <you@example.com>
2021-11-08 16:06:30 +0300

    update

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -3,19 +3,18 @@ import { Spin } from 'antd';
 import 'antd/dist/antd.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { initializeApplication } from '../redux/reducers/app-reducer';
-import { getApplicationInitialized, getUserAuthorization } from '../redux/selectors/selectors';
+import { getApplicationInitialized } from '../redux/selectors/selectors';
 import { Header } from '../Components/Header/Header';
 import Routes from '../routes/Routes';
 import './App.scss';
 
 export const App = () => {
   const initialized = useSelector(getApplicationInitialized);
-  const isLoggedIn = useSelector(getUserAuthorization);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(initializeApplication(isLoggedIn));
-  }, [dispatch, isLoggedIn]);
+    dispatch(initializeApplication());
+  }, [dispatch]);
 
   return !initialized
     ? <Spin style={{ position: 'absolute', top: '50%', left: '50%' }} size='large' />
